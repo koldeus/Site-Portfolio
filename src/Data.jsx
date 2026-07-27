af8aae8625e48781b54c99ffcc1c442e885fd077
oldeus <kenimottin@gmail.com>
2026-07-27 12:39:35 +0200

    changement cv

--- src/Data.jsx
+++ src/Data.jsx
@@ -29,7 +29,7 @@ export const content = {
           number: 15,
           label: "Technologies Maîtrisées",
         },
-        { icon: <Users size={32} />, number: 3, label: "Clients Satisfaits" },
+        { icon: <Users size={32} />, number: 0.5, label: "Clients Satisfaits" },
       ],
       skills: [
         "React",
@@ -192,7 +192,7 @@ export const content = {
           number: 15,
           label: "Technologies Mastered",
         },
-        { icon: <Users size={32} />, number: 3, label: "Satisfied Clients" },
+        { icon: <Users size={32} />, number: 0.5, label: "Satisfied Clients" },
       ],
       skills: [
         "React",
